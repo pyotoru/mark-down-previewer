b711fc4f00af7721563f165e05694d9323b44576
pyotoru <wawrzyniak.peter@gmail.com>
2021-05-29 13:06:00 +0900

    added header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
+import Badge from "react-bootstrap/Badge";
 
 export default class App extends React.Component {
 	render() {
-		return <div className="App">fd</div>;
-		<div></div>;
+		return (
+			<div className="App">
+				<div className="container">
+					<div className="row mt-4">
+						<div className="col text-center">
+							<h1>
+								<Badge
+									className="text-align-center"
+									variant="light"
+								>
+									Markdown Previewer
+								</Badge>
+							</h1>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
