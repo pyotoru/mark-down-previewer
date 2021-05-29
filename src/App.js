6428eecb316d3931f6b19b0290f92383ba433022
pyotoru <wawrzyniak.peter@gmail.com>
2021-05-29 10:11:15 +0900

    add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default class App extends React.Component {
 	render() {
-		return <div className="App"></div>;
+		return <div className="App">fd</div>;
 		<div></div>;
 	}
 }
