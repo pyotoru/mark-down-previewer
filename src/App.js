d88308e12102b800c55649c82b2ca8fb52d4282c
pyotoru <wawrzyniak.peter@gmail.com>
2021-05-30 22:19:54 +0900

    added textarea

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import Badge from "react-bootstrap/Badge";
 
 export default class App extends React.Component {
 	render() {
+		var inputStyle = {
+			width: "300px",
+			height: "50vh",
+			marginLeft: "auto",
+			marginRight: "auto",
+			padding: "10px",
+		};
+		var outputStyle = {
+			width: "300px",
+			height: "50vh",
+			backgroundColor: "#DCDCDC",
+			marginLeft: "auto",
+			marginRight: "auto",
+			padding: "10px",
+		};
+
 		return (
 			<div className="App">
 				<div className="container">
@@ -11,11 +27,43 @@ export default class App extends React.Component {
 							<h1>
 								<Badge
 									className="text-align-center"
-									variant="light"
+									variant="dark"
 								>
 									Markdown Previewer
 								</Badge>
 							</h1>
+							<div className="row mt-4">
+								<div className="col-md-6">
+									<h1>
+										<Badge
+											className="text-align-center"
+											variant="light"
+										>
+											Markdown
+										</Badge>
+									</h1>
+									<div className="mark-input">
+										<textarea
+											className="input"
+											style={inputStyle}
+										>
+											This is a markdown preview, do you
+											like it?
+										</textarea>
+									</div>
+								</div>
+								<div className="col-md-6">
+									<h1>
+										<Badge
+											className="text-align-center"
+											variant="light"
+										>
+											Preview
+										</Badge>
+									</h1>
+									<div style={outputStyle}></div>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
